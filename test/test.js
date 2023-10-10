c6e5bc4e44db46cad339ace295310211d5954dd9
Anuja Kale <62926535+Anuja-Kale@users.noreply.github.com>
2023-10-10 14:55:12 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const app = require('../app'); // Adjust the path as per your project structure
 describe('GET /healthz', () => {
   it('should return 200 and status ok when DB is connected', async () => {
     await request(app)
-      .get('/healthz')
+      .get('/healt')
       .expect('Cache-Control', 'no-cache, no-store, must-revalidate')
       .expect('Pragma', 'no-cache')
       .expect('X-Content-Type-Options', 'nosniff')
